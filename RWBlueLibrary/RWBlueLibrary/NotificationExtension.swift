c54adf91cab517249e30ee1a85bba04948ca2cd5
milicajan <milica24111991@live.com>
2019-02-22 00:30:49 +0100

    Add files via upload
    Design Pattern with RWBlueLibraryApp. MVC, Singletone, Facade, Decorator, Adapter, Observer, Memento

---
+++ RWBlueLibrary/RWBlueLibrary/NotificationExtension.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationExtension.swift
+//  RWBlueLibrary
+//
+//  Created by Milica Jankovic on 1/21/19.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+  
+  static let BLDownloadImage = Notification.Name("BLDownloadImageNotification")
+}
