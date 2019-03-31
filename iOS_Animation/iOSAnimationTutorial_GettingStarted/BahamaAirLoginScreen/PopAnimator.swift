2dd19086a6460557f3deea48ea55f13e9c7a32f7
milicajan <milica24111991@live.com>
2019-03-31 13:34:14 +0200

    Add files via upload

---
+++ iOS_Animation/iOSAnimationTutorial_GettingStarted/BahamaAirLoginScreen/PopAnimator.swift
@@ -0,0 +1,21 @@
+//
+//  PopAnimator.swift
+//  BahamaAirLoginScreen
+//
+//  Created by Milica Jankovic on 3/30/19.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+  func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+    return 0
+  }
+  
+  func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    
+  }
+  
+
+}
